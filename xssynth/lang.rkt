485333967d9e87f08ace2136e511a6398646af49
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-11-28 12:44:06 -0800

    Update syntax

--- xssynth/lang.rkt
+++ xssynth/lang.rkt
@@ -4,39 +4,66 @@
          rosette/lib/match)   ; provides `match`
 
 
-;; ----------------
-;; Syntax
+; ------
+; Syntax
+; ------
+
+; A tiny subset of xstream API
+;   * https://github.com/staltz/xstream
+
+
+; Stream as a list of events over discrete timesteps
+
+(struct empty-event () #:transparent)
+
+; simplified version of xstream's Stream
+;   * events is a list, e.g., '(1 (empty-event) 2 ...)
+;   * https://github.com/staltz/xstream#stream
+(struct stream (events) #:transparent)
+
+; simplified version of xstream's MemoryStream
+;   * init is a memory(-stream)'s initial value of and values is a list
+;     representing memory(-stream)'s current values
+;   * https://github.com/staltz/xstream#memorystream
+(struct memory (values [init #:auto]) #:transparent #:auto-value (empty-event))
+
+
+; Instruction as a register definition
 
 (struct instruction () #:transparent)
 (struct unary instruction (r1) #:transparent)          ; unary instruction
 (struct binary instruction (r1 r2) #:transparent)      ; binary instruction
 (struct ternary instruction (r1 r2 r3) #:transparent)  ; ternary instruction
-(struct program (inputs instructions) #:transparent)
 
-; Factories
+; a subset of xstream's Factories
+;   * https://github.com/staltz/xstream#factories
+(struct of unary () #:transparent)
 (struct merge binary () #:transparent)
 
-; Operators
+; a subset of xstream's Operators
+;   * https://github.com/staltz/xstream#operators
+(struct startWith binary () #:transparent)
+(struct map binary () #:transparent)
 (struct mapTo binary () #:transparent)
+(struct fold ternary () #:transparent)
+(struct remember unary () #:transparent)
 
 
+; Helpers
 
-;; Shorthands for instruction accessors
-
+; shortcuts for instruction accessors
 (define (r1 v)
   (match v
     [(unary f) f]
     [(binary f _) f]
     [(ternary f _ _) f]))
-
 (define (r2 v)
   (match v
     [(binary _ f) f]
     [(ternary _ f _) f]))
-
 (define r3 ternary-r3)
 
-; Returns true iff the given register is read by any of the given instructions
+; returns true iff the given register is read by any of the given instructions
 (define (used? reg insts)
   (ormap
    (lambda (inst)
@@ -52,14 +79,9 @@
 ;; Semantics
 
 ;; Stream
-
-(define NOEVENT 'no-evt)
-
-(define (empty-event? e)
-  (eq? NOEVENT e))
-
-(define (not-empty-event? e)
-  (not (eq? NOEVENT e)))
+(define (mergeE evt-stream1 evt-stream2)
+  (map (lambda (evt1 evt2) (if (empty-event? evt2) evt1 evt2))
+       evt-stream1 evt-stream2))
 
 (define (constantE const evt-stream)
   (map (lambda (x) (if (empty-event? x) 'no-evt const)) evt-stream))
