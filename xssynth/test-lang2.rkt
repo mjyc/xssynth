64dee08bb8124d27bf74c6c8b04fb0393accefc8
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-11-29 14:31:15 -0800

    Update test

--- xssynth/test-lang2.rkt
+++ xssynth/test-lang2.rkt
@@ -3,28 +3,59 @@
 (require quickcheck rackunit/quickcheck "lang2.rkt" rackunit rackunit/text-ui)
 
 
+; ------------------------------------------------------------------------------
+; Custom generators and arbitraries
+
 (define arbitrary-integer-or-boolean
   (arbitrary-mixed (list (cons '() arbitrary-integer)
                          (cons '() arbitrary-boolean)
                          )))
 
+(define generator-empty-event
+  (generator (lambda (i rgen) (empty-event))))
+
+(define arbitrary-empty-event
+  (arbitrary generator-empty-event
+             (lambda (x gen) gen)
+             ))
+
+(define arbitrary-integer-or-boolean-or-empty-event
+    (arbitrary-mixed (list (cons '() arbitrary-integer)
+                           (cons '() arbitrary-boolean)
+                           (cons '() arbitrary-empty-event)
+                           )))
+
+(define (arbitrary-tuple-events [tup-size 1]
+                                [arb arbitrary-integer-or-boolean-or-empty-event])
+  (arbitrary-list
+    (apply arbitrary-tuple
+      (build-list tup-size (lambda (x) arb)))))
+
+
+; ------------------------------------------------------------------------------
+; Tests
+
 (define (test-constant-interp)
   (test-case "test-constant-interp"
     (check-property
       (property ([int-or-bool arbitrary-integer-or-boolean])
-        (eqv? (constant-interp (constant int-or-bool))
+        (define c (constant int-or-bool))
+        (eqv? (constant-interp c)
               int-or-bool)))
   ))
-    ; (check-property
-    ;   (property ([int-or-bool arbitrary-integer-or-boolean])
-    ;     (eqv? (constant-interp (constant int-or-bool))
-    ;           int-or-bool)))
+
+(define (test-constant-$)
+  (test-case "test-constant-$"
+    (check-property
+      (property ([events (arbitrary-tuple-events)])
+        (define s (stream events))
+        (equal? ($-interp s) s)))
+  ))
 
 
 (define/provide-test-suite lang2-tests
   (test-constant-interp)
+  (test-constant-$)
   )
-
-; (run-tests-quiet lang2-tests)
 
 (run-tests lang2-tests)
