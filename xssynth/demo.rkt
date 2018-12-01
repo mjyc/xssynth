cb4b166b0bcc404e8e0857fd9c67f3b79914f563
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-12-01 15:54:36 -0800

    Update demo to run angelic execution too

--- xssynth/demo.rkt
+++ xssynth/demo.rkt
@@ -4,43 +4,68 @@
   "lang.rkt"
   "hole.rkt")
 
+(define inputsize 4)
+(define numinputs 2)
+
+
 
 (define spec
   (program
-    2
+    numinputs
     (list
       (xsmapTo (r 0) 1)
       (xsmapTo (r 1) -1)
       (xsmerge (r 2) (r 3))
+      ; (xsfold (r 4) + 0)
+      ; (xsmapTo (r 0) 1)
+      ; (xsfold (r 2) + 0)
       )))
 
-(displayln "Spec evaluation:")
-(program-interpret
-  spec
+(define test-inputs
   (list
     (list #t empty #t empty)
     (list empty #f empty #f)
     ))
 
+(displayln "Spec evaluation:")
+(program-interpret spec test-inputs)
 
-(define inputs
-  (??inputs (lambda (x) 'click) 4 2))
+
 
 (define sketch
   (program
-    (length inputs)
+    numinputs
     (build-list (length (program-instructions spec))
       (lambda (x) (??instruction)))
     ))
 
 (define M
+  (solve
+    (assert
+      (equal?
+        (program-interpret spec test-inputs)
+        (program-interpret sketch test-inputs)
+        )))
+  )
+(printf "~%Angelic execution:~%")
+(if (sat? M)
+  (evaluate sketch M)
+  (displayln "No program found"))
+
+
+
+(define sym-inputs
+  (??inputs (lambda (x) #t) inputsize numinputs))
+
+(define M2
   (synthesize
-    #:forall (symbolics inputs)
+    #:forall (symbolics sym-inputs)
     #:guarantee (assert (equal?
-      (program-interpret spec inputs)
-      (program-interpret sketch inputs)
+      (program-interpret spec sym-inputs)
+      (program-interpret sketch sym-inputs)
       ))))
 
-M
-(printf "~%Synthesized program:~%")
-(evaluate sketch M)
+(printf "~%Program synthesis:~%")
+(if (sat? M2)
+  (evaluate sketch M2)
+  (displayln "No program found"))
