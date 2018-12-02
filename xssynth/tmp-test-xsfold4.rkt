489ebb6964b142477f4b7b9521b5494db12aa9ea
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-12-01 22:00:09 -0800

    WIP: verification demo #2

--- xssynth/tmp-test-xsfold4.rkt
+++ xssynth/tmp-test-xsfold4.rkt
@@ -1,16 +1,16 @@
 #lang rosette
 
 (require
-  ; "lang.rkt" "hole.rkt"
+  "lang.rkt" "hole.rkt"
   ; (only-in rosette/query/debug define/debug debug) rosette/lib/render
   )
 
 
-; (define (spec input)
-;   (unoperator-interpret (xsmapTo input 1) null))
+(define (spec input)
+  (unoperator-interpret (xsmapTo input 1) null))
 
-; (define (sketch input)
-;   (binoperator-interpret (xsfold input + 0) null))
+(define (sketch input)
+  (unoperator-interpret (xsmapTo input 2) null))
 
 ; (displayln "Verification:")
 (define (??stream constructor size)
@@ -22,14 +22,15 @@
 (define sym-input
   (??stream (lambda () 0) 2))
 
-(map (lambda (x) (if (empty? x) 0 (add1 x))) sym-input)
+(sketch sym-input)
+(spec sym-input)
 
 (define cex (verify (assert (equal?
-  (map (lambda (x) (if (empty? x) 0 (add1 x))) sym-input)
-  (map (lambda (x) (if (empty? x) 0 (sub1 x))) sym-input)))))
+  (spec sym-input)
+  (sketch sym-input)))))
 cex
-
 (evaluate sym-input cex)
+
 ; (define cex-sym-input (evaluate sym-input cex))
 ; (spec cex-sym-input)
 ; (sketch cex-sym-input)
