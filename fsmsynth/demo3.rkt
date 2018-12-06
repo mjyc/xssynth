9e12c4274a7695b1beab88d254d10722f8159ed6
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-12-05 16:17:18 -0800

    WIP: can't make sense of the verification output

--- fsmsynth/demo3.rkt
+++ fsmsynth/demo3.rkt
@@ -43,12 +43,22 @@
     answers
   ))
 
+; (define test-inputs
+;   (list
+;     (list
+;       (input 'speechsynth-done '()) ; first question done & wait for answer
+;       (input 'speechrecog-done 'no)  ; heard yes, moving to "M1"
+;       (input 'speechsynth-done '())  ; You are an ice cream lover!
+;       (input 'speechrecog-done 'yes)
+;       )
+;     )
+;   )
 (define test-inputs
   (list
     (list
-      (input 'speechsynth-done '()) ; first question done & wait for answer
       (input 'speechrecog-done 'no)  ; heard yes, moving to "M1"
-      (input 'speechsynth-done '())  ; You are an ice cream lover!
+      (input 'speechrecog-done 'no)  ; heard yes, moving to "M1"
+      (input 'speechsynth-done '()) ; first question done & wait for answerr!
       (input 'speechrecog-done 'yes)
       )
     )
@@ -72,7 +82,7 @@
 (displayln "")
 
 (define numinputs 1)
-(define inputsize 8)
+(define inputsize 4)
 
 (define sym-inputs
   (list
@@ -88,6 +98,8 @@
             )
           ])
     )))
+sym-inputs
+(displayln "")
 
 (define ??trans-tbl (??transition-table monologues questions))
 (define ??trans2-tbl (??qa-transition-table questions answers monologues))
@@ -101,26 +113,57 @@
       (cons 'monologue 1)
       (cons 'monologue 2)
       )
-    ; (list  ; Q&A trans-tbl
-    ;   (list  ; Q0,
-    ;     (cons 'monologue 0)  ; A0
-    ;     (cons 'question 1) ; A1
-    ;   )
-    ;   (list  ; Q1,
-    ;     (cons 'monologue 1)  ; A0
-    ;     (cons 'monologue 2) ; A1
-    ;   )
-    ;   )
-    ??trans2-tbl
+    (list  ; Q&A trans-tbl
+      (list  ; Q0,
+        (cons 'monologue 0)  ; A0
+        (cons 'question 1) ; A1
+      )
+      (list  ; Q1,
+        (cons 'monologue 1)  ; A0
+        (cons 'monologue 2) ; A1
+      )
+      )
+    ; ??trans2-tbl
     monologues
     questions
     answers
   ))
 
-(define M
-  (synthesize
-    #:forall (symbolics sym-inputs)
-    #:guarantee (assert (equal?
+
+; (define M
+;   (synthesize
+;     #:forall (symbolics sym-inputs)
+;     #:guarantee (assert (equal?
+;       ; full spec
+;       (program-interpret (program
+;         numinputs
+;         (list
+;           (xsfold (r 0) trans (model 'question (variables 0) (create-outputs (list-ref questions 0))))
+;           )) sym-inputs)
+;       ; sketch
+;       (program-interpret (program
+;         numinputs
+;         (list
+;           (xsfold (r 0) ??trans (model 'question (variables 0) (create-outputs (list-ref questions 0))))
+;           )) sym-inputs)
+;       )))
+;   )
+
+; (printf "~%Program synthesis:~%")
+; (if (sat? M)
+;   ; M
+;   ; (print-forms M)
+;   (evaluate ??trans2-tbl M)  ; result has symbolic variables, this is because monologue happens at the end and doesn't really matter what happens afterwards
+;   (displayln "No program found"))
+
+
+; ------------------------------------------------------------------------------
+; Try verification
+(displayln "")
+(displayln "")
+
+(define cex (verify
+  (assert (equal?
       ; full spec
       (program-interpret (program
         numinputs
@@ -131,14 +174,16 @@
       (program-interpret (program
         numinputs
         (list
-          (xsfold (r 0) ??trans (model 'question (variables 0) (create-outputs (list-ref questions 0))))
+          (xsfold (r 0) trans (model 'question (variables 0) (create-outputs (list-ref questions 0))))
           )) sym-inputs)
-      )))
-  )
+      ))
 
-(printf "~%Program synthesis:~%")
-(if (sat? M)
-  ; M
-  ; (print-forms M)
-  (evaluate ??trans2-tbl M)  ; result has symbolic variables, this is because monologue happens at the end and doesn't really matter what happens afterwards
-  (displayln "No program found"))
+  ))
+
+(printf "~%Verification:~%")
+cex
+(if (sat? cex)
+  ; cex
+  ; (print-forms cex)
+  (evaluate sym-inputs cex)
+  (displayln "No counterexample found"))
